bd19aa7b8c5109f14d79d1189970050e38ed5f96
vithika <vgvithikagupta1@gmail.com>
2025-02-16 23:42:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import json from './data.json'
 
+const ListData = ({ list, addNodeToList,deleteNodeFromList }) => {
+  const [isExpanded, setIsExpanded] = useState({})
+  return (
+    <div className="container">
+      {list.map((node) => (
+        <div key={node.index}>
+          {node.isFolder && (
+            <span
+              onClick={() =>
+                setIsExpanded((prev) => ({
+                  ...prev,
+                  [node.name]: !prev[node.name],
+                }))
+              }
+            >
+              {' '}
+              {!isExpanded?.[node.name] ? '▸' : '▾'}
+            </span>
+          )}
+          <span>{node.name} </span>
+          {node?.isFolder && (
+            <span onClick={() => addNodeToList(node.index)}>
+              <img
+                className="add-folder"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHS6tLr2_N-wrENchm2pcwO8XH1ye1TEWJ20aXnLcjQBk0idlxWAdjUh7wwlXLuVJ076k&usqp=CAU"
+                alt="add"
+              ></img>
+             
+            </span>
+          )}
+          <span onClick={() => deleteNodeFromList(node.index)}>
+           <img
+                className="delete-folder"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmxlKWquCALZn0ICkdexTU_ELaRrCikd2kQA&s"
+                alt="add"
+              ></img>
+              </span>
+          {isExpanded?.[node.name] && node?.children && (
+            <ListData list={node.children} addNodeToList={addNodeToList} deleteNodeFromList={deleteNodeFromList} />
+          )}
+        </div>
+      ))}
+    </div>
+  )
+}
 function App() {
+  const [data, setData] = useState(json)
+
+  const addNodeToList = (parentId) => {
+    alert(parentId)
+    const name = prompt('Enter name')
+
+    const updateTree = (list) => {
+      return list.map((node) => {
+        if (node.index === parentId) {
+          return {
+            ...node,
+            children: [
+              ...node.children,
+              { index: Date.now().toString(), name: name, isFolder: true, children: [] },
+            ],
+          }
+        }
+        if (node.children) {
+          return { ...node, children: updateTree(node.children) }
+        }
+        return node
+      })
+    }
+    setData((prev) => updateTree(prev))
+  }
+  const deleteNodeFromList =(itemId)=>
+  {
+const updateTree =(list)=>
+{
+return list.filter(node=> node.index !== itemId)
+.map((node)=> {
+  if(node.children){
+    return {...node,children:updateTree(node.children)};
+  }
+  return node;
+});
+}
+setData(prev => updateTree(prev));
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Folder-Explorer
+      <ListData list={data} addNodeToList={addNodeToList} deleteNodeFromList={deleteNodeFromList}></ListData>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
